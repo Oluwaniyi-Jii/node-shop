62993e20fe45eac05542216b81d5c725a3cc39df
Oluwaniyi-Jii <79464168+Oluwaniyi-Jii@users.noreply.github.com>
2022-07-06 21:15:13 +0100

    added handlebars templating

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', engine({extname: "hbs", defaultLayout: "main-layout"}));
+app.set('view engine', 'hbs')
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
