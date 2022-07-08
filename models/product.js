217e82f956a79e5fba57af6653565eb52547e9db
Oluwaniyi-Jii <79464168+Oluwaniyi-Jii@users.noreply.github.com>
2022-07-08 23:35:17 +0100

    refactored the project model

--- models/product.js
+++ models/product.js
@@ -1,43 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-const products = []
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title){
-        this.title = title
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save(){
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err) {
-                products = JSON.parse(fileContent)
-            }
-            products.push(this)
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            })
-        })
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(cb){
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent)=> {
-            if(err){
-                cb([])
-            }
-            cb(JSON.parse(fileContent))
-        })
-        return products
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
